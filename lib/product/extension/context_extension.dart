77bd105ac36c715d328ee25fc68b7c9217e64498
Koray Liman <koraylimancre@gmail.com>
2023-07-29 23:20:43 +0300

    Tap to close keyboard improved

--- lib/product/extension/context_extension.dart
+++ lib/product/extension/context_extension.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 extension ContextExtension on BuildContext {
-  double get keyboardHeight => mediaQueryViewInsets.bottom;
-  bool get isKeyboardOn => keyboardHeight != 0;
+  bool get isKeyboardOn => FocusManager.instance.primaryFocus?.hasFocus == true;
   void turnOffKeyboard() {
     if (isKeyboardOn) {
       FocusManager.instance.primaryFocus?.unfocus();
